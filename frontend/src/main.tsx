b30b13e41e8c4fcf2f87bc46812e0f724822974c
charchit-chauhan <charchitc5752@gmail.com>
2026-09-12 14:58:34 +0530

    debug env var

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('API BASE URL:', import.meta.env.VITE_API_BASE_URL);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
